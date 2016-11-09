97dab4b9c0f6818a972460f25985b9a9b8f333e8
Vivek <vivek_d306@hotmail.com>
2016-11-09 19:23:23 +0000

    Add project files.

---
+++ ServiceInterface/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ServiceInterface.Interfaces;
+using ServiceInterface.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ServiceInterface
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private IMeVitae service;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            service = new MeVitae();
+        }
+
+        private void OutputText_Loaded(object sender, RoutedEventArgs e)
+        {
+            OutputText.Text = service.Add(2, 2).ToString();
+        }
+    }
+}
---
+++ ServiceInterface/Services/MeVitae.cs
@@ -0,0 +1,17 @@
+﻿using ServiceInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceInterface.Services
+{
+    class MeVitae : IMeVitae
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
